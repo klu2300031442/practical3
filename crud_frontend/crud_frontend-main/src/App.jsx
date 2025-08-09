b58b4973e2a028897e7ba1159df7e82a0f5e774f
PACHARU LAKSHMI KANTH <2300031442@kluniversity.in>
2025-08-09 09:42:51 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://16.170.172.240 :9090/springapp1';
+const BASE_URL = 'http://51.20.92.77:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -49,7 +49,7 @@ const BASE_URL = 'http://16.170.172.240 :9090/springapp1';
  /*
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://localhost:9090/delete/${id}`);
+      await axios.delete(`http://51.20.92.77:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
